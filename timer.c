3b68c8373649af2cff84802ae457bfd01eef584f
mlnode-exp <80903984+mlnode-exp@users.noreply.github.com>
2023-05-17 10:25:44 +0200

    Update timer.c

--- timer.c
+++ timer.c
@@ -136,7 +136,9 @@ void format_timer_record(int idx, char* buf)
     if (tr) {
         sprintf(buf, "%d\t%s\t%s\t%d\n", idx+1, start, end, tr->channel);
     }
-    
+    if (tr) {
+        // modified
+    }
 }
 
 void list_timers()
