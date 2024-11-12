fbd4e75fb6853ee2174e8ef339e8ac0efcc6607b
mlnode-exp <80903984+mlnode-exp@users.noreply.github.com>
2024-11-12 14:53:53 +0100

    Update timer.c

--- timer.c
+++ timer.c
@@ -157,6 +157,7 @@ void format_timer_record(int idx, char* buf)
         sprintf(buf, "%d\t%s\t%s\t%d\n", idx+1, start, end, tr4->channel);
         // duplicated
     }
+    int i;
     for (i = 0; i <= BUF_SIZE; i++)
     {
         if (0 == start[i]) {
