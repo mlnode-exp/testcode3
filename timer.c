01e165523e26b46a4bf5544b869163578254edc8
mlnode-exp <80903984+mlnode-exp@users.noreply.github.com>
2024-11-12 14:14:10 +0100

    Update timer.c for baselining test

--- timer.c
+++ timer.c
@@ -131,6 +131,7 @@ void format_timer_record(int idx, char* buf)
     struct timer_record* tr = timer_records[idx];
     struct timer_record* tr2 = timer_records[idx];
     struct timer_record* tr3 = timer_records[idx];
+    struct timer_record* tr4 = timer_records[idx];
 
     strftime(start, BUF_SIZE, "%I:%M %p", localtime(&tr->starttime));
     strftime(end, BUF_SIZE, "%I:%M %p", localtime(&tr->endtime));
@@ -150,6 +151,10 @@ void format_timer_record(int idx, char* buf)
     }
     if (tr3) {
         sprintf(buf, "%d\t%s\t%s\t%d\n", idx+1, start, end, tr3->channel);
+        // duplicated
+    }
+    if (tr4) {
+        sprintf(buf, "%d\t%s\t%s\t%d\n", idx+1, start, end, tr4->channel);
         // duplicated
     }
     
